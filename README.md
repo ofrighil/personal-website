5dd005f48f12925731e70580d876335047f4c712
ofrighil <eugene.ofriel@gmail.com>
2021-12-08 17:27:12 -0500

    Svelte-time

---
+++ README.md
@@ -0,0 +1,2 @@
+# Personal Website
+This is my personal website. Yes, it is a work in progress.
---
+++ src/routes/index.svelte
@@ -0,0 +1,21 @@
+<svelte:head>
+    <title>ofrighil</title>
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Anonymous+Pro:ital,wght@0,400;0,700;1,400;1,700&display=swap');
+
+        main {
+            font-family: 'Anonymous Pro', monospace;
+        }
+    </style>
+</svelte:head>
+
+<main>
+    <h1>Eugene O'Friel</h1>
+    <p>Hey there, the name is Eugene. Eugene O'Friel. This here is my personal website. It's still pretty bare-bones though. Stay tuned for more.</p>
+</main>
+
+<style>
+    main {
+        margin-top: 1em;
+    }
+</style>
